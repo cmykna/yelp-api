77502a3616e8ce0cfaff9cad02287831a483fca3
Chris Cykana <chris@cykana.com>
2011-11-09 13:19:27 -0600

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YelpApi::Application.config.secret_token = 'c56d1a5c1a20d78daa5618ddca57cba967282a19a5316469424997f14e857b5e9403cb4045fe31ebcc9edb89203c6f6f6032cc23a22c3e72a8c94c8dfc6eefeb'
